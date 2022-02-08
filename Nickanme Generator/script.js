b16e5ea1c8c66fa4ebcfab3f710e932654198fa2
TIdyrice-Laptop <cheneycai2004@hotmail.com>
2022-02-08 10:53:32 -0700

    School 2022-02-08

--- Nickanme Generator/script.js
+++ Nickanme Generator/script.js
@@ -1,15 +1,23 @@
 //get button
-document.getElementById("generate").addEventListener("click", calculate);
+document.getElementById("generate").addEventListener("click", Random);
+document.getElementById("listAll").addEventListener("click", ListAll);
+
+//get nickname text field
+const textField = document.getElementById("nickname");
 
 //array with nickanmes
 const nicknames = ["The Shadow", "Babe", "Honey", "Baby Bruiser", "Sexy Dog", "Lame Goldfish", "Coolboy", "Major", "Grandmother", "Papa", "Mama", "Smurf", "Alpha", "Breadmaker", "Bully", "Cereal Killer", "Nutjob", "Turnip King", "Guillotine", "Nerd", "Explicit Substance Dealer", "Cowboy"];
 
-//function to be executed on button press
-function calculate() {
+//random nickname function
+function Random() {
 
     //first and last name input fields
     let firstName = document.getElementById("first").value;
     let lastName = document.getElementById("last").value;
+
+    //if the input fields are empty, return a message
+    if (firstName == "" || lastName == "")
+        return textField.innerHTML = "Please enter your name!";
 
     //Random number generator
     let index = Math.floor(Math.random() * nicknames.length);
@@ -18,5 +26,28 @@ function calculate() {
     let nickname = `${firstName} "${nicknames[index]}" ${lastName}`;
 
     //output
-    document.getElementById("nickname").innerHTML = nickname;
+    textField.innerHTML = nickname;
+}
+
+//list all nicknames function
+function ListAll() {
+    
+    //first and last name input fields
+    let firstName = document.getElementById("first").value;
+    let lastName = document.getElementById("last").value;
+
+    //if the input fields are empty, return a message
+    if (firstName == "" || lastName == "")
+        return textField.innerHTML = "Please enter your name!";
+
+    //all the nickanmes will be put into this string
+    let allNicknames = "";
+
+    //loop to put all the nicknames into the string
+    for (i = 0; i < nicknames.length; i++) {
+        allNicknames += `${firstName} "${nicknames[i]}" ${lastName}<br>`
+    }
+
+    //output
+    textField.innerHTML = allNicknames;
 }
